868215654ea243ebb1dc9204a1ccb4f0d20c6bb7
sawanoboly <sawanoboriyu@higanworks.com>
2018-09-10 15:20:42 +0900

    add original source link for yoast-sitemap

--- shifter-wp-sitemaps.php
+++ shifter-wp-sitemaps.php
@@ -3,7 +3,7 @@
 Plugin Name: Shifter WP Sitemaps
 Plugin URI: https://github.com/getshifter/shifter-wp-sitemaps
 Description: Helper functions for WordPress sitemaps plugins on Shifter
-Version: 0.1.0
+Version: 0.1.1
 Author: DigitalCube
 Author URI: https://getshifter.io
 License: GPL-2.0
@@ -15,17 +15,18 @@ License: GPL-2.0
  * Add path for Yoast SEO sitemap_index.xml to WordPress Robots.txt
  *
  * @since  0.1.0
+ * Origin: https://github.com/Surbma/surbma-yoast-seo-sitemap-to-robotstxt
  */
 
 function shifter_wp_sitemaps_yoast( $output ) {
 
   $options = get_option( 'wpseo_xml' );
-  
+
   if ( class_exists( 'WPSEO_Sitemaps' ) && $options['enablexmlsitemap'] == true ) {
     $homeURL = get_home_url();
     $output .= "Sitemap: $homeURL/sitemap_index.xml\n";
   }
-  
+
   return $output;
 
 }
