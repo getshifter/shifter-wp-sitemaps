ddd8141b0ddb366b743c33ac584dfbed696d1d05
Daniel Olson <emaildano@gmail.com>
2019-02-18 12:58:20 -0500

    updated coding standards removed unused files

--- shifter-wp-sitemaps.php
+++ shifter-wp-sitemaps.php
@@ -1,32 +1,33 @@
 <?php
 /**
-Plugin Name: Shifter WP Sitemaps
-Package: WordPress_Custom_Fields_Permalink
-Plugin URI: https://github.com/getshifter/shifter-wp-sitemaps
-Description: Helper functions for WordPress sitemaps plugins on Shifter
-Version: 0.1.0
-Author: DigitalCube
-Author URI: https://getshifter.io
-License: GPL-2.0
+ * Sitemap
+ *
+ * Helper functions for sitemap WordPress plugins on Shifter
+ *
+ * @package shifter-wp-sitemaps
+ *
+ * @wordpress-plugin
+ * Plugin Name: Shifter WP Sitemaps
+ * PHP Version: 7
+ * Plugin URI: https://github.com/getshifter/shifter-wp-sitemaps
+ * Description: Helper functions for sitemap WordPress plugins on Shifter
+ * Version: 1.0.0
+ * Author: DigitalCube
+ * Author URI: https://getshifter.io
+ * License: GPL-2.0
  */
 
 /**
- * Yoast SEO
- * ---------
- * Add path for Yoast SEO sitemap_index.xml to WordPress Robots.txt
+ * Implements hook_help().
  *
- * @since  0.1.0
+ * @param string $output Adds Sitemap URL to Robots.txt.
  */
-
-function shifter_wp_sitemaps_yoast( $output )
-
-
-{
+function shifter_wp_sitemaps_yoast( $output ) {
 	$options = get_option( 'wpseo_xml' );
 
-	if ( class_exists( 'WPSEO_Sitemaps' ) && $options['enablexmlsitemap'] == true ) {
-		$homeURL = get_home_url();
-		$output .= "Sitemap: $homeURL/sitemap_index.xml\n";
+	if ( class_exists( 'WPSEO_Sitemaps' ) && true === $options['enablexmlsitemap'] ) {
+		$home_url = get_home_url();
+		$output  .= "Sitemap: $home_url/sitemap_index.xml\n";
 	}
 	return $output;
 }
