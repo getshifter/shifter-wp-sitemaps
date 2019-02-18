abdf1df4a1dd72eef07642daff61d7639f4e8cc2
Daniel Olson <emaildano@gmail.com>
2019-02-18 11:37:00 -0500

    testing wpcs

--- shifter-wp-sitemaps.php
+++ shifter-wp-sitemaps.php
@@ -1,13 +1,14 @@
 <?php
-/*
+/**
 Plugin Name: Shifter WP Sitemaps
+Package: WordPress_Custom_Fields_Permalink
 Plugin URI: https://github.com/getshifter/shifter-wp-sitemaps
 Description: Helper functions for WordPress sitemaps plugins on Shifter
 Version: 0.1.0
 Author: DigitalCube
 Author URI: https://getshifter.io
 License: GPL-2.0
-*/
+ */
 
 /**
  * Yoast SEO
@@ -18,16 +19,13 @@ License: GPL-2.0
  */
 
 function shifter_wp_sitemaps_yoast( $output ) {
+	$options = get_option( 'wpseo_xml' );
 
-  $options = get_option( 'wpseo_xml' );
-  
-  if ( class_exists( 'WPSEO_Sitemaps' ) && $options['enablexmlsitemap'] == true ) {
-    $homeURL = get_home_url();
-    $output .= "Sitemap: $homeURL/sitemap_index.xml\n";
-  }
-  
-  return $output;
-
+	if ( class_exists( 'WPSEO_Sitemaps' ) && $options['enablexmlsitemap'] == true ) {
+		$homeURL = get_home_url();
+		$output .= "Sitemap: $homeURL/sitemap_index.xml\n";
+	}
+	return $output;
 }
 
 add_filter( 'robots_txt', 'shifter_wp_sitemaps_yoast', 9999, 1 );
